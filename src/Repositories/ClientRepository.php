7288e7eb24fd124e527de97f1b0c114c24d1f6e6
Conrad Dobbs <conrad.dobbs@writethatessay.org>
2024-01-22 15:18:02 +1300

    Update league/oauth2-server to 8.4.2

--- src/Repositories/ClientRepository.php
+++ src/Repositories/ClientRepository.php
@@ -3,26 +3,44 @@
 namespace AdvancedLearning\Oauth2Server\Repositories;
 
 use AdvancedLearning\Oauth2Server\Entities\ClientEntity;
+use AdvancedLearning\Oauth2Server\Models\Client;
 use function hash_equals;
 use League\OAuth2\Server\Repositories\ClientRepositoryInterface;
 use const PHP_EOL;
 
 class ClientRepository implements ClientRepositoryInterface
 {
+    protected $clients = [];
+
     /**
      * {@inheritdoc}
      */
-    public function getClientEntity($clientIdentifier, $grantType, $clientSecret = null, $mustValidateSecret = true)
+    public function getClientEntity($clientIdentifier)
     {
-        $client = \AdvancedLearning\Oauth2Server\Models\Client::get()->filter([
-           'Identifier' => $clientIdentifier
-        ])->first();
-
-
-        if ($mustValidateSecret && $client && !hash_equals($client->Secret, $clientSecret)) {
-            $client = null;
+        if (!isset($this->clients[$clientIdentifier])) {
+            $this->getClient($clientIdentifier);
         }
 
-        return $client && $client->hasGrantType($grantType) ? new ClientEntity($clientIdentifier, $client->Name, 'something') : null;
+        return !empty($this->clients[$clientIdentifier])  ? new ClientEntity($clientIdentifier, $this->clients[$clientIdentifier]->Name, 'something') : null;
+    }
+
+    public function validateClient($clientIdentifier, $clientSecret, $grantType)
+    {
+        if (!isset($this->clients[$clientIdentifier])) {
+            $this->getClient($clientIdentifier);
+        }
+
+        $client = $this->clients[$clientIdentifier];
+
+        return !(!$client || $clientSecret !== $client->Secret || !$client->hasGrantType($grantType));
+    }
+
+    protected function getClient($clientIdentifier)
+    {
+        $client = Client::get()->filter([
+            'Identifier' => $clientIdentifier
+        ])->first();
+
+        $this->clients[$clientIdentifier] = $client ?? null;
     }
 }
